f6be238f75c66b315770e415c8b553335a4218cc
ldyislucky <1278950466@qq.com>
2023-12-10 22:30:36 +0800

    orderservice启动失败的原因就是因为在模块中没有配置spring-maven插件，父工程中配置这个没有用，模块中配置才有用

---
+++ order-service/src/main/java/com/ldy/order/OrderApplication.java
@@ -0,0 +1,16 @@
+package com.ldy.order;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@SpringBootApplication
+@MapperScan("com.ldy.order.mapper")
+@ComponentScan({"com.ldy.order.mapper","com.ldy.order.service","com.ldy.order.entity","com.ldy.order.controller"})
+public class OrderApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrderApplication.class,args);
+    }
+}
