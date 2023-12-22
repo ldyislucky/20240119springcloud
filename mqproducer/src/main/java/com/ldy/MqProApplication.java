30168e3ca25064013e8e18cdf7274fcfc2bfd496
ldyislucky <1278950466@qq.com>
2023-12-22 19:28:22 +0800

    添加生产者mq模块，重新搭建一下pom

---
+++ mqproducer/src/main/java/com/ldy/MqProApplication.java
@@ -0,0 +1,11 @@
+package com.ldy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MqProApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MqProApplication.class,args);
+    }
+}
---
+++ mqproducer/src/test/java/Springtest.java
@@ -0,0 +1,21 @@
+import com.ldy.MqProApplication;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest(classes = MqProApplication.class)
+@RunWith(SpringRunner.class)//不加这个注解没法跑
+public class Springtest {
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+    @Test
+    public void t1(){
+        String queue = "ldyqueues1";
+        String str = "holle world!";
+        rabbitTemplate.convertAndSend(queue,str);
+        System.out.println(rabbitTemplate);
+    }
+}
