cb70e4c9a8b629bc36e15ccb119a4420c0c4692b
ldyislucky <1278950466@qq.com>
2024-01-16 10:21:59 +0800

    补充一下漏掉的类

---
+++ user-service/src/main/java/com/ldy/user/service/IAccountFreezeTblService.java
@@ -0,0 +1,16 @@
+package com.ldy.user.service;
+
+import com.ldy.user.entity.AccountFreezeTbl;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author author
+ * @since 2024-01-15
+ */
+public interface IAccountFreezeTblService extends IService<AccountFreezeTbl> {
+
+}
---
+++ user-service/src/main/java/com/ldy/user/service/impl/AccountFreezeTblServiceImpl.java
@@ -0,0 +1,25 @@
+package com.ldy.user.service.impl;
+
+import com.baomidou.mybatisplus.extension.toolkit.SqlRunner;
+import com.ldy.user.entity.AccountFreezeTbl;
+import com.ldy.user.mapper.AccountFreezeTblMapper;
+import com.ldy.user.service.IAccountFreezeTblService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * <p>
+ *  服务实现类
+ * </p>
+ *
+ * @author author
+ * @since 2024-01-15
+ */
+@Service
+public class AccountFreezeTblServiceImpl extends ServiceImpl<AccountFreezeTblMapper, AccountFreezeTbl> implements IAccountFreezeTblService {
+
+}
