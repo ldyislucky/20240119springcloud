32cd1cf79756de987b00ad0846a14061ba40dd2a
ldyislucky <1278950466@qq.com>
2024-10-31 18:25:25 +0800

    演示一下注解模式

--- other/springbootquartz/src/main/java/org/quarzboot/ldy/controller/ManyScheduled.java
+++ other/springbootquartz/src/main/java/org/quarzboot/ldy/controller/ManyScheduled.java
@@ -2,6 +2,9 @@ package org.quarzboot.ldy.controller;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.quarzboot.ldy.service.MyJob;
+import org.quarzboot.ldy.service.MyJob3;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +14,20 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequestMapping("quartzboot")
 public class ManyScheduled {
+    @Autowired
+    private MyJob3 myJob;
+
     @GetMapping("/t1")
     public void run() throws InterruptedException {
         /**
          * 加载配置以触发定时任务
          */
         new ClassPathXmlApplicationContext("classpath:application.xml");
+    }
+
+    @GetMapping("/zhujie")
+    public void run2() throws InterruptedException {
+        myJob.run();
     }
 
 }
--- other/springbootquartz/src/main/java/org/quarzboot/ldy/service/MyJob3.java
+++ other/springbootquartz/src/main/java/org/quarzboot/ldy/service/MyJob3.java
@@ -2,19 +2,18 @@ package org.quarzboot.ldy.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+
 @Slf4j
-@Service
-public class MyJob {
-    //@Scheduled()
+@Component
+public class MyJob3 {
+    @Scheduled(fixedDelay = 1000)
     public void run() throws InterruptedException {
-        log.error(Thread.currentThread().getName()+"===开始执行111");
+        log.error(Thread.currentThread().getName()+"===开始执行注解");
         System.out.println("It's time to run :" + new Date());
-        Thread.sleep(2000);
-        log.error(Thread.currentThread().getName()+"===结束执行111");
+        log.error(Thread.currentThread().getName()+"===结束执行注解");
     }
-
-
 }
